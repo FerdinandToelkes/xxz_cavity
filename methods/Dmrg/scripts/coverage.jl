4360984359c9c43622b9302fde0b7c0f8d3e0f90
Ferdinand Tölkes <ftoelkes@uni-bremen.de>
2026-01-16 16:51:19 +0100

    finished implementing coverage and added some more tests

--- methods/Dmrg/scripts/coverage.jl
+++ methods/Dmrg/scripts/coverage.jl
@@ -1,16 +1,30 @@
-# This script generates a coverage report in LCOV format for the Dmrg module
-# This can be used by the VS Code Coverage Gutters extension to display coverage info
+"""
+This script generates a coverage report in LCOV format for the Dmrg module
+This can be used by the VS Code Coverage Gutters extension to display coverage info
 
-# Use the command pallette "Coverage Gutters: Watch" to use the generated coverage.info file
-# to display coverage info directly in the editor
+Execute this script by running:
 
-# Execute this script by running:
-# julia_itensors --project=. scripts/coverage.jl
-# within the methods/Dmrg directory or from the REPL with 
-# include("scripts/coverage.jl")
+    julia_itensors --project=. scripts/coverage.jl
 
+within the methods/Dmrg directory or from the REPL with 
 
-using Coverage, Coverage.LCOV
+    julia> include("scripts/coverage.jl")
 
-cov = process_folder("src")
-LCOV.writefile("lcov.info", cov)
+Use the command pallette "Coverage Gutters: Watch" to use the generated lcov.info file
+to display coverage info directly in the editor
+"""
+
+using Pkg, Coverage, Coverage.LCOV
+
+# run tests with coverage
+Pkg.test(coverage=true)
+
+# gather coverage data from the target_dir and write to file_name
+target_dir = "src"
+file_name = "lcov.info"
+cov = process_folder(target_dir)
+LCOV.writefile(file_name, cov)
+
+# clean up .cov files
+clean_folder(target_dir)
+println("Coverage report generated at $(joinpath(pwd(), file_name))")
--- methods/Dmrg/test/construct_mpo_tests.jl
+++ methods/Dmrg/test/construct_mpo_tests.jl
@@ -7,10 +7,10 @@ using Random
     Random.seed!(1234)
 
     # test intended use for various system sizes and coefficients
-    for L in (2, 3, 5), coeff in (0.7, -1.2)
+    for L in (2, 3, 5), coeff in (0.7, -1.2), pauli in (:X, :Y, :Z)
         sites = siteinds("S=1/2", L)
-        H = Dmrg.pauli_sum_mpo(sites, coeff)
-        H_manual = Dmrg.pauli_sum_manual_mpo(sites, coeff)
+        H = Dmrg.pauli_sum_mpo(sites, coeff; pauli=pauli)
+        H_manual = Dmrg.pauli_sum_manual_mpo(sites, coeff; pauli=pauli)
 
         for _ in 1:3
             ψ = random_mps(sites; linkdims=10)
@@ -22,12 +22,41 @@ using Random
         end
     end
 
-    # test error handling for invalid inputs
+    # test error handling for invalid sites
     @test_throws ArgumentError Dmrg.pauli_sum_manual_mpo(siteinds("S=1", 3), 1.0)
     @test_throws ArgumentError Dmrg.pauli_sum_mpo(siteinds("S=1", 3), 1.0)
+    # test error handling for invalid pauli symbol
     @test_throws ArgumentError Dmrg.pauli_sum_manual_mpo(siteinds("S=1/2", 3), 1.0; pauli=:A)
     @test_throws ArgumentError Dmrg.pauli_sum_mpo(siteinds("S=1/2", 3), 1.0; pauli=:A)
+    # test error handling for too short chains
     @test_throws ArgumentError Dmrg.pauli_sum_manual_mpo(siteinds("S=1/2", 0), 1.0)
     @test_throws ArgumentError Dmrg.pauli_sum_mpo(siteinds("S=1/2", 0), 1.0)
 end
 
+
+@testset "Heisenberg MPO: OpSum vs manual construction" begin
+    Random.seed!(1234)
+
+    # test intended use for various system sizes and coefficients
+    for L in (2, 4, 6), J in (2.5, -0.2), Jz in (1.0, -1.0)
+        sites = siteinds("S=1/2", L)
+        H = Dmrg.heisenberg_mpo(sites, J, Jz)
+        H_manual = Dmrg.heisenberg_manual_mpo(sites, J, Jz)
+
+        for _ in 1:3
+            ψ = random_mps(sites; linkdims=10)
+            @test isapprox(
+                inner(ψ', H, ψ),
+                inner(ψ', H_manual, ψ);
+                atol=1e-12
+            )
+        end
+    end
+
+    # test error handling for invalid sites
+    @test_throws ArgumentError Dmrg.heisenberg_manual_mpo(siteinds("S=1", 3), 1.0, 1.0)
+    @test_throws ArgumentError Dmrg.heisenberg_mpo(siteinds("S=1", 3), 1.0, 1.0)
+    # test error handling for too short chains
+    @test_throws ArgumentError Dmrg.heisenberg_manual_mpo(siteinds("S=1/2", 1), 1.0, 1.0)
+    @test_throws ArgumentError Dmrg.heisenberg_mpo(siteinds("S=1/2", 1), 1.0, 1.0)
+end
